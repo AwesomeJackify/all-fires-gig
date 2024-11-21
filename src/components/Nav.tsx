52359afb8ceb87c42e60e6d69939737265f24a48
Jack Gong <jackgong48@gmail.com>
2024-11-21 21:19:56 +1300

    ADDED: nav and hooked up the buttons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Icon } from "@iconify/react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 import config from "../config.json";
 
 const Nav = () => {
+  const [showNav, setShowNav] = useState(false)
+  gsap.registerPlugin(useGSAP);
+
+  const tl = useRef<gsap.core.Timeline>();
+
+  const mobileNavRef = useRef<HTMLUListElement>(null);
+
+  const { contextSafe } = useGSAP(() => {
+    tl.current = gsap.timeline();
+
+    tl.current.to(mobileNavRef.current, {
+      height: "auto",
+      duration: 0.3,
+      ease: "power1.inOut",
+    })
+  });
+
+  const toggleTimeline = contextSafe(() => {
+    setShowNav(!showNav);
+  });
+
+  useEffect(() => {
+    if (showNav) {
+      tl.current && tl.current.play();
+    } else {
+      tl.current && tl.current.reverse();
+    }
+  }, [showNav]);
+
   return (
     <div>
       <div className="navbar bg-base-100 max-md:hidden py-4">
@@ -20,14 +51,23 @@ const Nav = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn btn-primary text-white">Call Us Today</a>
+          <a href={config.bookingLink} className="btn btn-primary text-white">Call Us Today</a>
         </div>
       </div>
-      <div className="flex justify-between items-center py-4 px-4 md:hidden sticky">
-        <a className="btn btn-ghost text-2xl">{config.businessName}</a>
-        <button className="btn btn-ghost">
-          <Icon icon="mdi:menu" className="text-5xl" />
-        </button>
+      <div className="flex flex-col">
+        <div className="flex justify-between items-center py-4 px-4 md:hidden sticky">
+          <a className="btn btn-ghost text-2xl">{config.businessName}</a>
+          <button className="btn btn-ghost" onClick={toggleTimeline}>
+            <Icon icon={showNav ? "mdi:close" : "mdi:menu"} className="text-5xl" />
+          </button>
+        </div>
+        <ul ref={mobileNavRef} className={`flex flex-col overflow-hidden h-0`}>
+          {config.pages.map((page, index) => (
+            <li key={index} className="py-4 px-8">
+              <a className="link link-hover" href={page.url}>{page.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
